f0726829165c64eb796500665ac80587e83f2aea
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2020-03-27 20:08:33 -0700

    initial commit

---
+++ third.js
@@ -0,0 +1,99 @@
+/* OBJECTS */
+
+const person = {
+  firstName: "Tyler",
+  lastName: "Villa",
+  age: 18,
+  ethnicity: "hispanic"
+};
+
+const person2 = {
+  firstName: "Realest",
+  lastName: "Yo",
+  age: 18,
+  ethnicity: "jamaican"
+};
+
+const book = {
+  title: "The Winter",
+  published: 1920,
+  author: {
+    firstName: "James",
+    lastName: "Spencer",
+    age: 43,
+    ethnicity: "white",
+    otherBooks: [
+      {
+        title: "The Summer",
+        published: 1918,
+        genre: "Horror"
+      },
+      {
+        title: "Fall",
+        published: 1934,
+        genre: "Suspense"
+      }
+    ]
+  }
+};
+
+/* ARRAYS */
+
+const people = [
+  {
+    firstName: "Realest",
+    lastName: "Yo",
+    age: 18,
+    ethnicity: "jamaican"
+  },
+  {
+    firstName: "Tyler",
+    lastName: "Villa",
+    age: 18,
+    ethnicity: "hispanic"
+  },
+  {
+    firstName: "Joseph",
+    lastName: "Lemire",
+    age: 18,
+    ethnicity: "hispanic"
+  },
+  {
+    firstName: "Bob",
+    lastName: "Marley",
+    age: "deceased",
+    ethnicity: "jamaican"
+  },
+  {
+    firstName: "Elijah",
+    lastName: "Paje",
+    age: 19,
+    ethnicity: "Filipino"
+  },
+  {
+    firstName: "Dante",
+    lastName: "Rivera",
+    age: 17,
+    ethnicity: "hispanic"
+  },
+  {
+    firstName: "Edward",
+    lastName: "Villa",
+    age: 30,
+    ethnicity: "hispanic"
+  },
+  {
+    firstName: "Christian",
+    lastName: "Villa",
+    age: 23,
+    ethnicity: "hispanic"
+  },
+  {
+    firstName: "Noel",
+    lastName: "Villa",
+    age: 42,
+    ethnicity: "hispanic"
+  }
+];
+
+console.log(people2);
